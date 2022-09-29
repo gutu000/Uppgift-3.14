4747da79f79891b20d828378336cc749fe70d025
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-29 13:59:02 +0200

    Add project files.

---
+++ Uppgift 3.14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Uppgift_3._13
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? j/n");
+            string svar = Console.ReadLine().ToLower();
+            
+            if (svar == "j")
+            {
+                Console.WriteLine("Bra");
+            }
+            if (svar == "n")
+            {
+                Console.WriteLine("Tyvär, du måste gå ut gymnasiet före");
+            }
+            
+            if (svar == "j")
+            {
+                Console.WriteLine("Hur gammal är du?");
+            }
+            int ålder = int.Parse(Console.ReadLine());
+            
+            if (ålder <= 21)
+            {
+                Console.WriteLine("Perfekt, vi vill anställa dig");
+            }
+            else if (ålder >= 22)
+            {
+                Console.WriteLine("Du är tyvär för gammal");
+            }
+
+
+
+             
+            
+
+               
+             
+            
+            
+        }
+    }
+}
